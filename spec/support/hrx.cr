950008d61c1a551b5652290d33ff6408c54662e1
Johannes Müller <straightshoota@gmail.com>
2025-03-18 14:42:43 +0100

    Add expectations for specific libxml2 versions
    Alternatively, we could define multiple outputs and expect one of them to match. The version restrictions are a bit more strict (not sure if that's necessary, though) and weren't too dificult to implement.

--- spec/support/hrx.cr
+++ spec/support/hrx.cr
@@ -1,5 +1,15 @@
 require "spec"
 require "hrx"
+require "semantic_version"
+
+lib LibXML
+  $xmlParserVersion : LibC::Char*
+end
+
+private def libxml_version
+  number = String.new(LibXML.xmlParserVersion).to_i
+  "#{number // 10_000}.#{number % 10_000 // 100}.#{number % 100}"
+end
 
 def run_hrx_samples_dir(dir)
   Dir.each_child(dir) do |child|
@@ -44,21 +54,49 @@ def run_hrx_samples(archive_file, policies, *, relative_to = __DIR__)
   end
 end
 
+private LIBXML2_VERSION = SemanticVersion.parse(libxml_version)
+
+def test_libxml_version(path)
+  md = path.match(/^libxml2(<|<=|==|>=|>)([^:]+)/) || return path
+
+  comparator = md[1]
+  version = SemanticVersion.parse(md[2])
+
+  case comparator
+  when "<"
+    return unless LIBXML2_VERSION < version
+  when "<="
+    return unless LIBXML2_VERSION <= version
+  when "=="
+    return unless LIBXML2_VERSION == version
+  when ">="
+    return unless LIBXML2_VERSION >= version
+  when ">"
+    return unless LIBXML2_VERSION > version
+  else
+    fail "Unrecgonized libxml2 version constraint: #{comparator.inspect} in #{path}"
+  end
+
+  path.byte_slice((md.byte_end + 1)..)
+end
+
 def it_hrx_sample(policies, source, expected, archive_file)
   extension = File.extname(expected.path)
-  basename = File.basename(expected.path, extension)
+  path = test_libxml_version(expected.path) || return
+  basename = File.basename(path, extension)
+
   found_policy = true
   policy = policies.fetch(basename) { found_policy = false; nil }
   if !policy && found_policy
-    pending "#{File.dirname(source.path)} #{basename}"
+    pending "#{File.dirname(source.path)} #{expected.path}"
     return
   end
 
-  it "#{File.dirname(source.path)} (#{basename})" do
+  it "#{File.dirname(source.path)} (#{expected.path})" do
     if p = policy
       assert_sanitize(p, source, expected, file: archive_file)
     else
-      raise "Unregistered policy #{basename}"
+      fail "Unregistered policy #{basename}"
     end
   end
 end
